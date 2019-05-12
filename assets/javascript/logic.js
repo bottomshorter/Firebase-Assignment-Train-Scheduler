388039cf4bfe0de09e71173ec6ef6880c974001f
bottomshorter <longtopsshortbottoms@gmail.com>
2019-05-11 17:13:39 -0700

    added firebase

---
+++ assets/javascript/logic.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    // Initialize Firebase
+    var firebaseConfig = {
+        apiKey: "AIzaSyAmTWSidUrpy4ITp5HtGJiB9SQusrV1U1g",
+        authDomain: "firemomenthw.firebaseapp.com",
+        databaseURL: "https://firemomenthw.firebaseio.com",
+        projectId: "firemomenthw",
+        storageBucket: "firemomenthw.appspot.com",
+        messagingSenderId: "1031198431819",
+        appId: "1:1031198431819:web:a065aa08a660c978"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+});
